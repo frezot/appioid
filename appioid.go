5d66a020101266a1665c09673742bea1fc8a7288
fserov <frezot@gmail.com>
2019-02-02 15:51:38 +0700

    second implementation

--- appioid.go
+++ appioid.go
@@ -9,37 +9,59 @@ import "os/exec"
 import "regexp"
 import "time"
 import "runtime"
+import "io/ioutil"
+import "flag"
 
-// State is an unexported type
-type State struct {
+// appiumState is an unexported type
+type appiumState struct {
 	free bool
 	dob  time.Time
 }
 
-var devicesPool = make(map[string]State)
-var portsPool = make(map[string]State)
-var bysyLimit = 180 * time.Second
-var unsupportedOsError = "Sory, not implemented for UNIX systems yet"
+// deviceState is an unexported type
+type deviceState struct {
+	free bool
+	port string
+	dob  time.Time
+}
+
+var devicesPool = make(map[string]deviceState)
+var appiumsPool = make(map[string]appiumState)
+var portCounter = 4724
+var busyLimit = 180 * time.Second
+var poolSize = 2
+var reservedDevice = "WilNotBeUsedInPool"
+var host = "http://127.0.0.1"
+var appiodPort = ":9090"
+var timeFormat = "15:04:05"
+var unsupportedOsError = "Sory, not implemented for UNIX systems yet 😰"
+var restartInProgress = false
 
 func defaultAction(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w,
 		"You can use this commands \n"+
 			"===========================================================\n"+
-			"http://127.0.0.1:9090/getDevice\n"+
-			"http://127.0.0.1:9090/releaseDevice?name={deviceName}\n"+
-			"http://127.0.0.1:9090/getPort\n"+
-			"http://127.0.0.1:9090/releasePort?port={number}\n"+
-			"http://127.0.0.1:9090/rereadDevices\n"+
-			"http://127.0.0.1:9090/forceCleanUp\n"+
+			host+appiodPort+"/getDevice\n"+
+			host+appiodPort+"/stopDevice?name={deviceName}\n"+
+
+			host+appiodPort+"/getAppium\n"+
+			host+appiodPort+"/stopAppium?port={number}\n"+
+
+			host+appiodPort+"/status\n"+
+			host+appiodPort+"/forceCleanUp\n"+
 			"===========================================================")
 }
 
 func debug(w http.ResponseWriter, r *http.Request) {
 	log.Println(devicesPool)
-	log.Println(portsPool)
+	log.Println(appiumsPool)
 }
 
-func releaseDevice(w http.ResponseWriter, r *http.Request) {
+func getDevice(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, discoverFreeDevice())
+}
+
+func stopDevice(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	target := ""
 	for k, v := range r.Form {
@@ -48,19 +70,18 @@ func releaseDevice(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if target == "" {
-		fmt.Fprintf(w, "INCORRECT REQUEST \nExpected form: \n_host_/releaseDevice?name={deviceName}")
+		fmt.Fprintf(w, "INCORRECT REQUEST \nExpected form: \n_host_/stopDevice?name={deviceName}")
 	} else {
-		_, matched := devicesPool[target]
-		if matched {
-			devicesPool[target] = State{free: true, dob: time.Now()}
-			fmt.Fprintf(w, "OK")
-		} else {
-			fmt.Fprintf(w, "UNKNOWN")
-		}
+		fmt.Fprintf(w, deviceSetFree(target))
 	}
 }
 
-func releasePort(w http.ResponseWriter, r *http.Request) {
+func getAppium(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, discoverFreeAppium())
+}
+
+func stopAppium(w http.ResponseWriter, r *http.Request) {
+
 	r.ParseForm()
 	target := ""
 	for k, v := range r.Form {
@@ -69,75 +90,153 @@ func releasePort(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if target == "" {
-		fmt.Fprintf(w, "INCORRECT REQUEST \nExpected form: \n_host_/releasePort?port={number}")
+		fmt.Fprintf(w, "INCORRECT REQUEST \nExpected form: \n_host_/stopAppium?port={number}")
 	} else {
-		_, matched := portsPool[target]
-		if matched {
-			portsPool[target] = State{free: true, dob: time.Now()}
-			cleanUpAppiumProcesses()
-			fmt.Fprintf(w, "OK")
-		} else {
-			fmt.Fprintf(w, "UNKNOWN")
-		}
+		fmt.Fprintf(w, appiumSetFree(target))
 	}
 }
 
-func getDevice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, findFreeDevice())
-}
-
-func getPort(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, findFreePort())
-}
-
-func rereadDevices(w http.ResponseWriter, r *http.Request) {
+func status(w http.ResponseWriter, r *http.Request) {
 	loadDevices()
 
-	fmt.Fprintf(w, "Actual devices list:\n")
-	fmt.Fprintf(w, "----------------------------\n")
-	for d := range devicesPool {
-		fmt.Fprintf(w, "| %-25s|\n", d)
+	fmt.Fprintf(w, "\nActual appiums list:\n")
+	fmt.Fprintf(w, "|==============URL==============|=free?=|\n")
+	for a := range appiumsPool {
+		fmt.Fprintf(w, "| %-30s| %-5t | %s\n", appiumServerURL(a), appiumsPool[a].free, appiumStatus(a))
 	}
-	fmt.Fprintf(w, "----------------------------")
+	fmt.Fprintf(w, "-----------------------------------------\n\n")
+
+	fmt.Fprintf(w, "Actual devices list:\n")
+	fmt.Fprintf(w, "|=============NAME==============|=port=|=free?=|\n")
+	for d := range devicesPool {
+		fmt.Fprintf(w, "| %-30s| %4s | %-5t |\n", d, devicesPool[d].port, devicesPool[d].free)
+	}
+	fmt.Fprintf(w, "------------------------------------------------\n\n")
 }
 
 func forceCleanUp(w http.ResponseWriter, r *http.Request) {
-	cleanUpAppiumProcesses()
+
+	begin := time.Now().Format(timeFormat)
+
+	log.Printf("==> [START] Force restart <==")
+	for port := range appiumsPool {
+		tryToRestart(port)
+	}
 	loadDevices()
+	for d := range devicesPool {
+		deviceSetFree(d)
+	}
+	log.Printf("==> [FINISH] Force restart <==")
+	end := time.Now().Format(timeFormat)
+	fmt.Fprintf(w, "⏳\t%s\n⏰\t%s\n✅ forceCleanUp is complete\n", begin, end)
+	status(w, r)
 }
 
-func findFreeDevice() string {
-	//TODO: actualise list here in case of some changes on PC
+func appiumSetFree(appiumname string) string {
+	_, matched := appiumsPool[appiumname]
+	if matched {
+		appiumsPool[appiumname] = appiumState{free: true, dob: time.Now()}
+		return "OK"
+	}
+	return "UNKNOWN PORT"
+}
+
+func appiumSetBusy(key string) {
+	appiumsPool[key] = appiumState{free: false, dob: time.Now()}
+}
+
+func deviceSetFree(devicename string) string {
+	_, matched := devicesPool[devicename]
+	if matched {
+		adbPort := devicesPool[devicename].port
+		killProcess(adbPort)
+		devicesPool[devicename] = deviceState{free: true, port: adbPort, dob: time.Now()}
+		return "OK"
+	}
+	return "UNKNOWN DEVICE NAME"
+}
+
+func deviceSetBusy(devicename string) {
+	adbPort := devicesPool[devicename].port
+	devicesPool[devicename] = deviceState{free: false, port: adbPort, dob: time.Now()}
+}
+
+func appiumStatus(port string) string {
+	response, err := http.Get(appiumServerURL(port) + "/status")
+	if err == nil {
+		defer response.Body.Close()
+		responseData, _ := ioutil.ReadAll(response.Body)
+		return string(responseData)
+	}
+	return "ERR"
+}
+
+func appiumIsReady(port string) bool {
+
+	for i := 0; i < 7; i++ {
+		if appiumStatus(port) == "ERR" {
+			time.Sleep(500 * time.Millisecond)
+		} else {
+			return true
+		}
+	}
+	return (appiumStatus(port) != "ERR")
+}
+
+func discoverFreeDevice() string {
+	loadDevices()
 	for name := range devicesPool {
-		if devicesPool[name].free == true || time.Now().Sub(devicesPool[name].dob) > bysyLimit {
-			devicesPool[name] = State{free: false, dob: time.Now()}
-			return name
+		if devicesPool[name].free {
+			deviceSetBusy(name)
+			return (name + " " + devicesPool[name].port)
 		}
 	}
-	return "PLEASE WAIT"
+	return "WAIT"
 }
 
-func findFreePort() string {
-	cleanUpAppiumProcesses()
-	for port := range portsPool {
-		if portsPool[port].free == true {
-			portsPool[port] = State{free: false, dob: time.Now()}
-			return port
-		} else if time.Now().Sub(portsPool[port].dob) > bysyLimit {
-			portsPool[port] = State{free: true, dob: time.Now()}
-			cleanUpAppiumProcesses()
+func discoverFreeAppium() string {
+	for port := range appiumsPool {
+		if appiumStatus(port) == "ERR" {
+			tryToRestart(port)
+			continue
+		}
+		if !appiumsPool[port].free && time.Now().Sub(appiumsPool[port].dob) > busyLimit {
+			tryToRestart(port)
+			continue
+		}
+		if appiumsPool[port].free {
+			appiumSetBusy(port)
+			return appiumServerURL(port)
 		}
 	}
-	return "PLEASE WAIT"
+	return "WAIT"
+}
+
+func appiumServerURL(port string) string {
+	return host + ":" + port + "/wd/hub"
+}
+
+func startAppiumNode(port string) {
+	cmd := exec.Command("appium", "-p", port, "--log-level", "error")
+	if err := cmd.Start(); err == nil {
+		if appiumIsReady(port) {
+			log.Printf("[DONE] appium started on " + port)
+			appiumsPool[port] = appiumState{true, time.Now()}
+		} else {
+			log.Printf("[ERROR] Failed to start appium on %s port", port)
+			killProcess(port)
+		}
+	} else {
+		log.Printf("[ERROR] Failed to start cmd: %v", err)
+	}
 }
 
 func initialLoad() {
-	//TODO: reads and apply portsPool size from env or args
-	portsPool["4724"] = State{true, time.Now()}
-	portsPool["4725"] = State{true, time.Now()}
-	portsPool["4726"] = State{true, time.Now()}
-	portsPool["4727"] = State{true, time.Now()}
-	cleanUpAppiumProcesses()
+
+	for i := 0; i < poolSize; i++ {
+		startAppiumNode(strconv.Itoa(portCounter))
+		portCounter++
+	}
 	loadDevices()
 }
 
@@ -150,46 +249,38 @@ func loadDevices() {
 
 	adbListing := make(map[string]struct{})
 
+	// stage1: find new devices and record to devicesPool
 	for _, elem := range devs {
 		devName := elem[1] // first group from regexp match
+		if devName == reservedDevice {
+			continue
+		}
 		adbListing[devName] = struct{}{}
 
 		_, registred := devicesPool[devName]
-		// if detected device yet not registred in pool -> write down
 		if !registred {
-			devicesPool[devName] = State{true, time.Now()}
+			devicesPool[devName] = deviceState{port: strconv.Itoa(portCounter), free: true, dob: time.Now()}
+			portCounter++
 		}
 	}
 
+	// stage2: find disconnected devices and remove from devicesPool
 	for devFromPool := range devicesPool {
 		_, online := adbListing[devFromPool]
-		// if device were disconnected -> remove from pool
 		if !online {
 			delete(devicesPool, devFromPool)
 		}
 	}
-}
 
-func cleanUpAppiumProcesses() {
-	for port := range portsPool {
-		if portsPool[port].free == true {
-			outlawPid := getProcessByPort(port)
-			if outlawPid != "" {
-				killPid(outlawPid)
-			}
-			//clean adb-processes too, they use another then appium port
-			//numbering rule is simple: appium + 3500 (e.g. 4724 -> 8229)
-			portAsInt, _ := strconv.Atoi(port)
-			adbPort := strconv.Itoa(portAsInt + 3500)
-			adbPid := getProcessByPort(adbPort)
-			if adbPid != "" {
-				killPid(adbPid)
-			}
+	// stage3: check for devices which is busy more than busyLimit
+	for actualDevice := range devicesPool {
+		if time.Now().Sub(devicesPool[actualDevice].dob) > busyLimit {
+			deviceSetFree(actualDevice)
 		}
 	}
 }
 
-func getProcessByPort(p string) string {
+func getPidByPort(p string) string {
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("netstat", "-ano")
 		out, _ := cmd.CombinedOutput()
@@ -207,30 +298,54 @@ func getProcessByPort(p string) string {
 }
 
 func killPid(id string) {
+	if id == "" {
+		return // no pid -- no action
+	}
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("taskkill", "/F", "/pid", id)
 		_, err := cmd.CombinedOutput()
 		if err == nil {
-			log.Println("[DONE] taskkill /pid " + id)
+			log.Println("[DONE] taskkill /F /pid " + id)
 		}
 	} else {
 		log.Fatal(unsupportedOsError)
 	}
 }
 
+func killProcess(port string) {
+	killPid(getPidByPort(port))
+}
+
+func tryToRestart(appiumPort string) {
+	if restartInProgress {
+		log.Println("tryToRestart(" + appiumPort + ") failed. Locked")
+	} else {
+		restartInProgress = true
+		killProcess(appiumPort)
+		startAppiumNode(appiumPort)
+		restartInProgress = false
+	}
+}
+
 func main() {
+
+	flag.IntVar(&poolSize, "s", 1, "How much appium servers should works at same time")
+	flag.StringVar(&reservedDevice, "d", "", "You can set the device-name which shouldnt be used")
+	flag.Parse()
 
 	http.HandleFunc("/", defaultAction)
 	http.HandleFunc("/debug", debug)
 	http.HandleFunc("/getDevice", getDevice)
-	http.HandleFunc("/releaseDevice", releaseDevice)
-	http.HandleFunc("/getPort", getPort)
-	http.HandleFunc("/releasePort", releasePort)
-	http.HandleFunc("/rereadDevices", rereadDevices)
+	http.HandleFunc("/stopDevice", stopDevice)
+
+	http.HandleFunc("/getAppium", getAppium)
+	http.HandleFunc("/stopAppium", stopAppium)
+
+	http.HandleFunc("/status", status)
 	http.HandleFunc("/forceCleanUp", forceCleanUp)
 
 	initialLoad()
-	err := http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(appiodPort, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
